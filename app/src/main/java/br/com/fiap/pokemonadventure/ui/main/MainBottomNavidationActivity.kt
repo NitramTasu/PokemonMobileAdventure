397598af9dc9dd77476a89faa6fa6dfb9d470322
Martin Sch <amrastasu@gmail.com>
2019-04-30 16:41:38 -0300

    Implemented basic Push Notification.

--- app/src/main/java/br/com/fiap/pokemonadventure/ui/main/MainBottomNavidationActivity.kt
+++ app/src/main/java/br/com/fiap/pokemonadventure/ui/main/MainBottomNavidationActivity.kt
@@ -3,10 +3,16 @@ package br.com.fiap.pokemonadventure.ui.main
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import br.com.fiap.pokemonadventure.R
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.iid.FirebaseInstanceId
 import kotlinx.android.synthetic.main.activity_main_bottom_navidation.*
 
 class MainBottomNavidationActivity : AppCompatActivity() {
+
+    val TAG = "FirebaseToken"
 
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -35,5 +41,21 @@ class MainBottomNavidationActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main_bottom_navidation)
 
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+
+        FirebaseInstanceId.getInstance().instanceId
+            .addOnCompleteListener(OnCompleteListener { task ->
+                if (!task.isSuccessful) {
+                    Log.w(TAG, "getInstanceId failed", task.exception)
+                    return@OnCompleteListener
+                }
+
+                // Get new Instance ID token
+                val token = task.result?.token
+
+                // Log and toast
+                val msg = "Token $token criado!"
+                Log.d(TAG, msg)
+                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+            })
     }
 }
